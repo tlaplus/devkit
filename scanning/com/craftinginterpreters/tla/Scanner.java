dc1c383b6d588c054d257184abe7cf849066398b
Andrew Helwer <ahelwer@pm.me>
2025-04-01 12:07:42 -0400

    Fixed bug in scanner
    Signed-off-by: Andrew Helwer <ahelwer@pm.me>

--- scanning/com/craftinginterpreters/tla/Scanner.java
+++ scanning/com/craftinginterpreters/tla/Scanner.java
@@ -127,7 +127,7 @@ class Scanner {
   static {
     keywords = new HashMap<>();
     keywords.put("ELSE",       ELSE);
-    keywords.put("ENABLED",    ELSE);
+    keywords.put("ENABLED",    ENABLED);
     keywords.put("FALSE",      FALSE);
     keywords.put("IF",         IF);
     keywords.put("THEN",       THEN);
@@ -161,7 +161,7 @@ class Scanner {
 
   private void number() {
     while (isDigit(peek())) advance();
-    addToken(NAT_NUMBER,
+    addToken(NUMBER,
         Integer.parseInt(source.substring(start, current)));
   }
 
