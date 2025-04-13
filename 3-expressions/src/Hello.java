608693bbf648b7d8844823cb088bac6b14989ecd
Andrew Helwer <ahelwer@pm.me>
2025-04-13 11:01:59 -0400

    Basic SBT build
    Signed-off-by: Andrew Helwer <ahelwer@pm.me>

---
+++ 3-expressions/src/Hello.java
@@ -0,0 +1,7 @@
+package src;
+
+class Hello {
+  public static void main(String... args) {
+    System.out.println("Hello, world!");
+  }
+}
---
+++ 3-expressions/test/TestHello.java
@@ -0,0 +1,12 @@
+package src;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class TestHello {
+  @Test
+  public void Test() {
+    assertTrue(true);
+    Hello.main();
+  }
+}
